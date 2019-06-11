11f319da108ff5be25a1d2142f8211d92176331a
everan-1994 <everan@aliyun.com>
2019-06-11 12:43:31 +0800

    add test

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -45,9 +45,9 @@ class AuthenticationController extends Controller
     {
         $code = $request->input('code');
 
-        // $access_token = $this->app->oauth->getAccessToken($code);
+         $access_token = $this->app->oauth->getAccessToken($code);
 
-        $wx_user = $this->app->oauth->user()->getOriginal()->toArray();
+        $wx_user = $this->app->oauth->user($access_token)->getOriginal()->toArray();
 
         return response()->json($wx_user);
     }
