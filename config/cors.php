2bf3c29769a7ab1fa4e10272ea4d33ec46002907
everan-1994 <everan@aliyun.com>
2019-07-01 01:47:09 +0800

    update api

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'supportsCredentials' => false,
+    'supportsCredentials' => true,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
